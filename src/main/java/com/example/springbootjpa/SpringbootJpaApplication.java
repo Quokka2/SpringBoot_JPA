2d52990cbe53fd4074745684b608c049f40aa865
home <gkadudgnl@gmail.com>
2023-08-29 10:30:34 +0900

    JPA start

--- src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -1,13 +1,27 @@
 package com.example.springbootjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.orm.jpa.EntityManagerFactoryUtils;
+
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
-public class SpringbootJpaApplication {
+public class SpringbootJpaApplication implements CommandLineRunner {
 
+    @Autowired
+    EntityManagerFactory emf;
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootJpaApplication.class, args);
+       SpringApplication app = new SpringApplication(SpringbootJpaApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("emf = " + emf);
+    }
 }
