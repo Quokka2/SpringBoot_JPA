7544f26c4fdb28adf7888df83b0f3020b143028c
home <gkadudgnl@gmail.com>
2023-08-30 09:06:54 +0900

    JPA

--- src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
@@ -22,6 +25,26 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        EntityManager em = emf.createEntityManager();
         System.out.println("emf = " + emf);
+        System.out.println("em = " + em);
+        EntityTransaction tx = em.getTransaction();
+
+        User user = new User();
+        user.setId("aaa");
+        user.setPassword("1234");
+        user.setName("Lee");
+        user.setEmail("aaa@aaa.com");
+        user.setInDate(new Date());
+        user.setUpDate(new Date());
+
+        tx.begin();
+        em.persist(user);
+
+        user.setPassword("4321");
+        tx.commit();
+
+        User user2 = em.find(User.class, "aaa");
+        System.out.println("(user == user2) = " + (user == user2));
     }
 }
